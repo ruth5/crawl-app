623139235fad2d2a0006e7c3ace2eabf3c6a7f8a
Ruth Rosenthal <ruthbrosenthal@gmail.com>
2021-10-13 04:05:04 +0000

    started getting directions to work with hardcoded locations

--- static/js/showRoute.js
+++ static/js/showRoute.js
@@ -1,30 +1,39 @@
 'use strict';
 
 function initMap() {
+    const routeCoords = [];
     const humphreyCoords = {
-        lat: 37.75303451932446, 
+        lat: 37.75303451932446,
         lng: -122.41187293853972,
     };
+    routeCoords.push(humphreyCoords);
 
     const biRiteCoords = {
-        lat: 37.76175113473264, 
+        lat: 37.76175113473264,
         lng: -122.42566335756337,
     };
 
+    routeCoords.push(biRiteCoords);
     const houseOfPancakesCoords = {
-        lat: 37.7430759633752, 
+        lat: 37.7430759633752,
         lng: -122.47613712502925,
     };
+    routeCoords.push(houseOfPancakesCoords);
+
 
     const burmaSuperStarCoords = {
-        lat: 37.7837765579607, 
+        lat: 37.7837765579607,
         lng: -122.46273936046609,
     };
+    routeCoords.push(burmaSuperStarCoords);
+
 
     const yankSingCoords = {
-        lat: 37.79279235853008, 
+        lat: 37.79279235853008,
         lng: -122.39297750174063,
     };
+    routeCoords.push(yankSingCoords);
+
 
     const cords = [humphreyCoords, biRiteCoords, houseOfPancakesCoords, burmaSuperStarCoords, yankSingCoords];
 
@@ -33,37 +42,59 @@ function initMap() {
         zoom: 13,
     });
 
-    const markers = []
-    for (const cord of cords) {
-        markers.push(
-            new google.maps.Marker({
-                position: cord
-            })
-        )
-        console.log(cord);
+    // const markers = []
+    // for (const cord of cords) {
+    //     markers.push(
+    //         new google.maps.Marker({
+    //             position: cord,
+    //             map: map,
+    //         })
+    //     )
+
+    // }
+
+    // for (const marker of markers) {
+    //     const markerInfo = `
+    //     <p>
+    //         Coordinates of this test marker:
+    //         ${marker.position.lat()}
+    //         ${marker.position.lng()}
+    //     </p>
+    //     `
+    //         ;
+
+
+    //     const infoWindow = new google.maps.InfoWindow({
+    //         content: markerInfo,
+    //         maxWidth: 200,
+    //     });
+
+    //     marker.addListener('click', () => {
+    //         infoWindow.open(map, marker)
+    //     })
+    // }
+
+    const directionsService = new google.maps.DirectionsService();
+
+    // Directions Renderer draws directions on the map
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    directionsRenderer.setMap(map);
+
+    const crawlRoute = {
+        origin: humphreyCoords,
+        destination: biRiteCoords,
+        waypoints: [{location: burmaSuperStarCoords}],
+        travelMode: 'DRIVING',
     }
-    console.log(markers);
 
-    for (const marker of markers) {
-        const markerInfo = `
-        <p>
-            Hi
-            ${marker.position.lat()}
-            ${marker.position.lng()}
-        </p>
-        `
-        ;
-    
-
-    const infoWindow = new google.maps.InfoWindow({
-        content: markerInfo,
-        maxWidth: 200,
+    directionsService.route(crawlRoute, (response, status) => {
+        if (status === 'OK') {
+            directionsRenderer.setDirections(response);
+        }
+        else {
+            alert(`Directions request unsuccessful due to ${status}`)
+        }
     });
-
-    marker.addListener('click', () => {
-        infoWindow.open(map, marker)
-    })
-}
 
 
 
