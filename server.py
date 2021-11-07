01a4783e4d1de38253ff9fae91ccbbbf758dfe32
Ruth Rosenthal <ruthbrosenthal@gmail.com>
2021-11-07 00:18:07 +0000

    added navigate.py file and moved navigation functions there from server to improve code organization

--- server.py
+++ server.py
@@ -3,6 +3,7 @@
 from flask import (Flask, render_template, request, flash, session, redirect, jsonify)
 from model import connect_to_db
 import crud
+from navigate import (get_coordinates, get_places)
 import os
 import requests
 import json
@@ -56,63 +57,6 @@ def login_user():
             flash("The password you provided is not correct")
 
     return redirect('/')
-
-def get_coordinates(location):
-    """Get coordinates for a given location."""
-
-    url = "https://maps.googleapis.com/maps/api/geocode/json"
-    payload = {'address': location, 'key': GOOGLE_API_KEY}
-    req = requests.get(url, params=payload)
-    req_info = req.json()
-    coordinates = req_info["results"][0]["geometry"]["location"]
-
-    formatted_coords = f"""{coordinates['lat']},{coordinates['lng']}"""
-
-
-    return formatted_coords
-
-
-def get_places(coordinates = '37.7749%2C-122.4194'):
-    """Get places from the places API."""
-    # Try grabbing locations from the Places API on server side
-
-    radius = '1500'
-    place_type = 'bakery'
-    keyword = 'dessert'
-
-    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-
-    payload = {'location': coordinates, 'radius': radius, 'type': place_type, 'keyword': keyword, 'key': GOOGLE_API_KEY}
-
-    req = requests.get(url, params=payload)
-    print(req.url)
-
-    place_list = req.json()
-
-    return place_list
-
-def make_nearest_neighbor_route(locations_set):
-    current_stop = locations_set.pop()
-    stop_number = 1
-    #add current stop as route location to database
-
-    min_duration = 100000
-    while len(locations_set) > 1:
-        for location in locations_set:
-            duration = calc_duration(current_stop.google_place_id, location.google_place_id)
-            if min_duration > duration:
-                min_duration = duration
-                nearest_neighbor = location
-        current_stop = nearest_neighbor
-        stop_number += 1
-        #add current stop as route location to database
-        locations_set.remove(current_stop)
-    final_stop = locations_set.pop()
-    stop_number+= 1
-    # add final stop to db
-
-
-    pass
 
 
 
