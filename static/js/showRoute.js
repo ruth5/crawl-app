8dd5ab5e18ae71b5f644cda0ab4af468b4cd04c9
Ruth Rosenthal <ruthbrosenthal@gmail.com>
2022-01-10 01:44:03 +0000

    clean up javascript files

--- static/js/showRoute.js
+++ static/js/showRoute.js
@@ -1,9 +1,6 @@
 'use strict';
 
-
-
 function initMap() {
-    const routeCoords = [];
 
     const form = document.querySelector('#routeinfo');
     let map = new google.maps.Map(document.querySelector('#map'), {
@@ -25,30 +22,28 @@ function initMap() {
         const numStops = document.querySelector('#num-stops').value
 
         if (routeZipCode) {
-            const queryString = new URLSearchParams({location: routeZipCode, radius: radius, keyword: routeKeyword, place_type: placeType, stops: numStops}).toString();
+            const queryString = new URLSearchParams({ location: routeZipCode, radius: radius, keyword: routeKeyword, place_type: placeType, stops: numStops }).toString();
             console.log(queryString)
             fetch(`/api/routes?${queryString}`)
                 .then((response) => response.json())
                 .then((responseJSON => {
                     if ("status" in responseJSON) {
                         if (responseJSON["status"] === "error") {
-                            console.log("there was an error");
                             const map_placeholder = document.querySelector('#map');
                             map_placeholder.innerHTML = "<p> There were not enough places with your criteria for us to make a crawl route. Please try another search. </p>";
                         }
-                        
+
                     }
                     else {
-                        console.log(responseJSON)
                         map = new google.maps.Map(document.querySelector('#map'), {
                             center: responseJSON["locations"][0]["coords"],
-    
+
                             zoom: 13,
                         });
-                    
-                    
+
+
                         const directionsService = new google.maps.DirectionsService();
-                    
+
                         // Directions Renderer draws directions on the map
                         const directionsRenderer = new google.maps.DirectionsRenderer(
                             {
@@ -56,21 +51,21 @@ function initMap() {
                             }
                         );
                         directionsRenderer.setMap(map);
-    
+
 
                         const places = responseJSON["locations"]
-    
-                        
+
+
                         const first_stop = places[0]["place_id"]
                         const last_stop = places[places.length - 1]["place_id"]
-    
+
                         const waypoints = []
                         for (let i = 1; i < (places.length - 1); i += 1) {
                             let new_waypoint = {}
-                            new_waypoint["location"] = {placeId: places[i]["place_id"]}
+                            new_waypoint["location"] = { placeId: places[i]["place_id"] }
                             waypoints.push(new_waypoint)
                         };
-                    
+
                         const crawlRoute = {
                             origin: { placeId: first_stop },
                             destination: { placeId: last_stop },
@@ -78,33 +73,32 @@ function initMap() {
                             travelMode: 'DRIVING',
                         }
 
-                        console.log(crawlRoute)
-                    
+
                         directionsService.route(crawlRoute, (response, status) => {
                             if (status === 'OK') {
                                 directionsRenderer.setDirections(response);
                                 const stopInfo = new google.maps.InfoWindow()
 
-                                for (let i=0; i < places.length; i++) {
+                                for (let i = 0; i < places.length; i++) {
 
-                                const stopMarker = new google.maps.Marker({
-                                    position: places[i]["coords"],
-                                    label: `${i + 1}`,
-                                    map: map,
+                                    const stopMarker = new google.maps.Marker({
+                                        position: places[i]["coords"],
+                                        label: `${i + 1}`,
+                                        map: map,
                                     });
-                                
-
-                                stopMarker.addListener('click', () => {
-                                    stopInfo.close();
-                                    stopInfo.setContent(`<h5> Crawl Stop #${i + 1} </h5> <p class="info-window-name"> ${places[i]["name"]} </p>`);
-                                    stopInfo.open(map, stopMarker);
-                                });
-                            document.querySelector('#save-crawl').style.display = '';
-                            } 
 
 
+                                    stopMarker.addListener('click', () => {
+                                        stopInfo.close();
+                                        stopInfo.setContent(`<h5> Crawl Stop #${i + 1} </h5> <p class="info-window-name"> ${places[i]["name"]} </p>`);
+                                        stopInfo.open(map, stopMarker);
+                                    });
+                                    document.querySelector('#save-crawl').style.display = '';
+                                }
 
-    
+
+
+
                             }
                             else {
                                 alert(`Directions request unsuccessful due to ${status}`)
@@ -112,38 +106,12 @@ function initMap() {
                         });
                     }
 
-                    
+
                 }
                 ))
         }
     }
     );
-        // Adds a marker to the map.
-    function addInfoWindow(content) {
-        new google.maps.InfoWindow({
-            content: content,
-        });
-    }
-    function addMarker(location, map) {
-        new google.maps.Marker({
-        position: location,
-        map: map,
-        });
-    }
 
 
 }
-
-// function saveRoute() {
-//     saveRouteForm = document.querySelector('#save-crawl');
-
-//     saveRouteForm.addEventListener("submit", (evt) => {
-//         evt.preventDefault();
-//         const crawlName = document.querySelector('#crawl-name').value;
-//         if (crawlName) {
-
-//         }
-
-
-//     })
-// }
--- static/js/showSavedRoute.js
+++ static/js/showSavedRoute.js
@@ -3,7 +3,6 @@
 
 
 function initMap() {
-    const routeCoords = [];
 
     let map = new google.maps.Map(document.querySelector('#map'), {
         center: {
@@ -18,126 +17,93 @@ function initMap() {
         buttons[i].addEventListener('click',
             () => {
                 let buttonID = buttons[i].id
-                console.log(buttonID)
                 fetch(`/api/saved-routes/${buttonID}`)
-                .then((response) => response.json())
-                .then((responseJSON => {
-                    if ("status" in responseJSON) {
-                        if (responseJSON["status"] === "error") {
-                            console.log("there was an error");
-                            const map_placeholder = document.querySelector('#map');
-                            map_placeholder.innerHTML = "<p> There were not enough places with your criteria for us to make a crawl route. Please try another search. </p>";
+                    .then((response) => response.json())
+                    .then((responseJSON => {
+                        if ("status" in responseJSON) {
+                            if (responseJSON["status"] === "error") {
+                                const map_placeholder = document.querySelector('#map');
+                                map_placeholder.innerHTML = "<p> There were not enough places with your criteria for us to make a crawl route. Please try another search. </p>";
+                            }
+
                         }
-                        
-                    }
-                    else {
-                        console.log(responseJSON)
-                        map = new google.maps.Map(document.querySelector('#map'), {
-                            center: responseJSON["locations"][0]["coords"],
-    
-                            zoom: 13,
-                        });
-                    
-                    
-                        const directionsService = new google.maps.DirectionsService();
-                    
-                        // Directions Renderer draws directions on the map
-                        const directionsRenderer = new google.maps.DirectionsRenderer(
-                            {
-                                suppressMarkers: true
-                            }
-                        );
-                        directionsRenderer.setMap(map);
-    
+                        else {
+                            map = new google.maps.Map(document.querySelector('#map'), {
+                                center: responseJSON["locations"][0]["coords"],
 
-                        const places = responseJSON["locations"]
-    
-                        
-                        const first_stop = places[0]["place_id"]
-                        const last_stop = places[places.length - 1]["place_id"]
-    
-                        const waypoints = []
-                        for (let i = 1; i < (places.length - 1); i += 1) {
-                            let new_waypoint = {}
-                            new_waypoint["location"] = {placeId: places[i]["place_id"]}
-                            waypoints.push(new_waypoint)
-                        };
-                    
-                        const crawlRoute = {
-                            origin: { placeId: first_stop },
-                            destination: { placeId: last_stop },
-                            waypoints: waypoints,
-                            travelMode: 'DRIVING',
+                                zoom: 13,
+                            });
+
+
+                            const directionsService = new google.maps.DirectionsService();
+
+                            // Directions Renderer draws directions on the map
+                            const directionsRenderer = new google.maps.DirectionsRenderer(
+                                {
+                                    suppressMarkers: true
+                                }
+                            );
+                            directionsRenderer.setMap(map);
+
+
+                            const places = responseJSON["locations"]
+
+
+                            const first_stop = places[0]["place_id"]
+                            const last_stop = places[places.length - 1]["place_id"]
+
+                            const waypoints = []
+                            for (let i = 1; i < (places.length - 1); i += 1) {
+                                let new_waypoint = {}
+                                new_waypoint["location"] = { placeId: places[i]["place_id"] }
+                                waypoints.push(new_waypoint)
+                            };
+
+                            const crawlRoute = {
+                                origin: { placeId: first_stop },
+                                destination: { placeId: last_stop },
+                                waypoints: waypoints,
+                                travelMode: 'DRIVING',
+                            }
+
+                            directionsService.route(crawlRoute, (response, status) => {
+                                if (status === 'OK') {
+                                    directionsRenderer.setDirections(response);
+                                    const stopInfo = new google.maps.InfoWindow()
+
+                                    for (let i = 0; i < places.length; i++) {
+
+                                        const stopMarker = new google.maps.Marker({
+                                            position: places[i]["coords"],
+                                            label: `${i + 1}`,
+                                            map: map,
+                                        });
+
+
+                                        stopMarker.addListener('click', () => {
+                                            stopInfo.close();
+                                            stopInfo.setContent(`<h5> Crawl Stop #${i + 1} </h5> <p class="info-window-name"> ${places[i]["name"]} </p>`);
+                                            stopInfo.open(map, stopMarker);
+                                        });
+                                    }
+
+
+
+
+                                }
+                                else {
+                                    alert(`Directions request unsuccessful due to ${status}`)
+                                }
+                            });
                         }
 
-                        console.log(crawlRoute)
-                    
-                        directionsService.route(crawlRoute, (response, status) => {
-                            if (status === 'OK') {
-                                directionsRenderer.setDirections(response);
-                                const stopInfo = new google.maps.InfoWindow()
 
-                                for (let i=0; i < places.length; i++) {
-
-                                const stopMarker = new google.maps.Marker({
-                                    position: places[i]["coords"],
-                                    label: `${i + 1}`,
-                                    map: map,
-                                    });
-                                
-
-                                stopMarker.addListener('click', () => {
-                                    stopInfo.close();
-                                    stopInfo.setContent(`<h5> Crawl Stop #${i + 1} </h5> <p class="info-window-name"> ${places[i]["name"]} </p>`);
-                                    stopInfo.open(map, stopMarker);
-                                });
-                            } 
-
-
-
-    
-                            }
-                            else {
-                                alert(`Directions request unsuccessful due to ${status}`)
-                            }
-                        });
                     }
-
-                    
-                }
-                ))
+                    ))
 
             }
         )
 
     }
 
-    function addInfoWindow(content) {
-        new google.maps.InfoWindow({
-            content: content,
-        });
-    }
-    function addMarker(location, map) {
-        new google.maps.Marker({
-            position: location,
-            map: map,
-        });
-    }
-
-
-
 }
-
-// function saveRoute() {
-//     saveRouteForm = document.querySelector('#save-crawl');
-
-//     saveRouteForm.addEventListener("submit", (evt) => {
-//         evt.preventDefault();
-//         const crawlName = document.querySelector('#crawl-name').value;
-//         if (crawlName) {
-
-//         }
-
-
-//     })
-// }
